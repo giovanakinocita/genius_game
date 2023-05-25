6f484903d21ebc00164fce883fd6e1aaf3c265e4
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-05-25 16:18:27 -0300

    Acrescentando mais LED's

--- genius.cpp
+++ genius.cpp
@@ -1,12 +1,20 @@
 
-#define LED_VERMELHO 2
-#define UM_SEGUNDO 500
+#define LED_VERDE 2
+#define LED_AMARELO 3
+#define LED_VERMELHO 4
+#define LED_AZUL 5
+
+#define UM_SEGUNDO 1000
 #define MEIO_SEGUNDO 500
 
 void setup()
 {
     Serial.begin(9600);
+
+    pinMode(LED_VERDE, OUTPUT);
+    pinMode(LED_AMARELO, OUTPUT);
     pinMode(LED_VERMELHO, OUTPUT);
+    pinMode(LED_AZUL, OUTPUT);
 
 }
 
@@ -14,7 +22,11 @@ void loop()
 {
 
     Serial.println(LED_VERMELHO);
+
+    piscaLed(LED_VERDE);
+    piscaLed(LED_AMARELO);
     piscaLed(LED_VERMELHO);
+    piscaLed(LED_AZUL);
 
 }
 
